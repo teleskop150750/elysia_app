44454040ad9862b9b88f943f9a81cdad23643b1c
teleskop150750 <teleskop150750@gmail.com>
2026-01-12 16:08:34 +0300

    chore

--- src/api/filters/index.ts
+++ src/api/filters/index.ts
@@ -1,35 +1,55 @@
 import { Elysia } from "elysia";
 import { db } from "~/db/drizzle.ts";
 
-async function getMarks() {
+const DemoFilter = {
+  model: {
+    mark: {
+      country: {
+        id: {
+          in: ["01988432-d400-773f-b544-9b6ab4e714d5"],
+        },
+      },
+    },
+  },
+};
+
+async function getMarks(filter: any) {
   return db.query.AutoMarksTable.findMany({
     extras: {
       value: (table, { sql }) => sql`${table.id}`,
       label: (table, { sql }) => sql`${table.name}`,
     },
     columns: {},
-    limit: 20,
+    where: filter,
+    limit: 5,
   });
 }
-async function getModels() {
+async function getModels(filter: any) {
   return db.query.AutoModelsTable.findMany({
     extras: {
       value: (table, { sql }) => sql`${table.id}`,
       label: (table, { sql }) => sql`${table.name}`,
     },
     columns: {},
-    limit: 20,
+    where: filter,
+    limit: 5,
   });
 }
 
-export const filter = new Elysia()
-  .all("/filter/options/base", () => {
-    const tableName = "marks";
-    return tableName === "marks" ? getMarks() : getModels();
-  })
-  .all("/filter/options/marks", () => {
-    return getMarks();
-  })
-  .all("/filter/options/models", () => {
-    return getModels();
-  });
+function getFiltersForField<T>(field: string, filter: T): T {
+  // исключить из фильтра те фильтры, которые относятся к полю field
+  // например, для field = "mark" нужно исключить из фильтра все, что внутри model.mark
+  // Чтобы запрос выдобавал все доступные марки, которые пользователь может выбрать на фронтенде
+  return filter;
+}
+
+export const filter = new Elysia().all("/filter/options/demo", async () => {
+  const [marks, models] = await Promise.all([
+    getMarks(getFiltersForField("mark", DemoFilter.model.mark)),
+    getModels(getFiltersForField("model", DemoFilter.model)),
+  ]);
+  return {
+    marks,
+    models,
+  };
+});

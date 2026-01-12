4b28c6a49135eaff2414c5a9fb4f51639f58e891
teleskop150750 <teleskop150750@gmail.com>
2026-01-12 14:25:58 +0300

    chore

--- src/api/filters/index.ts
+++ src/api/filters/index.ts
@@ -1,24 +1,35 @@
 import { Elysia } from "elysia";
 import { db } from "~/db/drizzle.ts";
 
+async function getMarks() {
+  return db.query.AutoMarksTable.findMany({
+    extras: {
+      value: (table, { sql }) => sql`${table.id}`,
+      label: (table, { sql }) => sql`${table.name}`,
+    },
+    columns: {},
+    limit: 20,
+  });
+}
+async function getModels() {
+  return db.query.AutoModelsTable.findMany({
+    extras: {
+      value: (table, { sql }) => sql`${table.id}`,
+      label: (table, { sql }) => sql`${table.name}`,
+    },
+    columns: {},
+    limit: 20,
+  });
+}
+
 export const filter = new Elysia()
+  .all("/filter/options/base", () => {
+    const tableName = "marks";
+    return tableName === "marks" ? getMarks() : getModels();
+  })
   .all("/filter/options/marks", () => {
-    return db.query.AutoMarksTable.findMany({
-      extras: {
-        value: (table, { sql }) => sql`${table.id}`,
-        label: (table, { sql }) => sql`${table.name}`,
-      },
-      columns: {},
-      limit: 20,
-    });
+    return getMarks();
   })
   .all("/filter/options/models", () => {
-    return db.query.AutoModelsTable.findMany({
-      extras: {
-        value: (table, { sql }) => sql`${table.id}`,
-        label: (table, { sql }) => sql`${table.name}`,
-      },
-      columns: {},
-      limit: 20,
-    });
+    return getModels();
   });
